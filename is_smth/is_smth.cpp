3301b1bdbcbfa144d7635f7a051ca8b0d7bb16e7
nvardp21 <nvardp21@gmail.com>
2021-06-02 17:04:39 +0400

    Add project files.

---
+++ is_smth/is_smth.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <type_traits>
+
+#pragma region is_void
+template<typename T>
+struct is_void
+{
+    static const bool value = false;
+};
+template<>
+struct is_void<void>
+{
+    static const bool value = true;
+};
+#pragma endregion
+
+#pragma region is_default_constructible
+
+namespace my {
+    template <class T, typename T2 = T>
+    struct is_default_constructible
+    {
+        static const bool value = false;
+    };
+
+    template <class T>
+    struct is_default_constructible<T, decltype(T())>
+    {
+        static const bool value = true;
+    };
+
+    template <>
+    struct is_default_constructible<void, void>
+    {
+        static const bool value = false;
+    };
+
+};
+#pragma endregion
+
+struct A {
+    A() = default;
+};
+struct B {
+    B() = delete;
+};
+
+int main()
+{
+    std::cout << my::is_default_constructible<A>::value;
+    std::cout << my::is_default_constructible<B>::value;
+    std::cout << my::is_default_constructible<void>::value;
+    std::cout << std::is_default_constructible<A>::value;
+    std::cout << std::is_default_constructible<B>::value;
+    std::cout << std::is_default_constructible<void>::value;
+}
